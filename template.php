1ff300e5f7eb51d3572104285ab34189c4aa0b57
palagpatski <palagpatski@gmail.com>
2018-07-10 14:07:13 +0800

    Add breadcrumb separators

--- template.php
+++ template.php
@@ -5,3 +5,17 @@
  * Template overrides as well as (pre-)process and alter hooks for the
  * WWM Marina Omega theme.
  */
+
+function wwm_marina_omega_breadcrumb($variables) {
+  $breadcrumb = $variables['breadcrumb'];
+
+  if (count($variables['breadcrumb']) > 1) {  // Only display the breadcrumb if there are more than one items in the trail
+    // Provide a navigational heading to give context for breadcrumb links to
+    // screen-reader users. Make the heading invisible with .element-invisible.
+    $output = '<h2 class="element-invisible">' . t('You are here') . '</h2>';
+
+    // Override Drupal core breadcrumb item imploding (no arrow).
+    $output .= '<ul class="breadcrumb"><li>' . implode('</li> » <li>', $breadcrumb) . '</li></ul>';
+    return $output;
+  }
+}
