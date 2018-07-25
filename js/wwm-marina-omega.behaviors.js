a5f2408e92878d284e56456f29e790386ceaca93
palagpatski <palagpatski@gmail.com>
2018-07-25 14:51:59 +0800

    Make post views responsive and equal height

--- js/wwm-marina-omega.behaviors.js
+++ js/wwm-marina-omega.behaviors.js
@@ -57,4 +57,72 @@
     }
   };
 
+  Drupal.behaviors.responsiveEqualHeight = {    
+    attach: function (context, settings) {
+      equalheight = function(container){
+
+        var currentTallest = 0,
+            currentRowStart = 0,
+            rowDivs = new Array(),
+            $el,
+            topPosition = 0;
+        $(container).each(function() {
+
+          $el = $(this);
+          $($el).height('auto')
+          topPostion = $el.position().top;
+
+          if (currentRowStart != topPostion) {
+            for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+              rowDivs[currentDiv].height(currentTallest);
+            }
+            rowDivs.length = 0; // empty the array
+            currentRowStart = topPostion;
+            currentTallest = $el.height();
+            rowDivs.push($el);
+          } else {
+            rowDivs.push($el);
+            currentTallest = (currentTallest < $el.height()) ? ($el.height()) : (currentTallest);
+          }
+          for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+            rowDivs[currentDiv].height(currentTallest);
+          }
+        });
+      }
+
+      $(document).ready(function() {
+        if ($(window).width() > 943) {
+          equalheight('.featured-display .views-row');
+        }
+        else {
+          $('.featured-display .views-row').css({
+            'height': 'auto',
+          })
+        }
+      });
+
+      $(document).on("DOMNodeInserted", function() {
+        if ($(window).width() > 943) {
+          equalheight('.featured-display .views-row');
+        }
+        else {
+          $('.featured-display .views-row').css({
+            'height': 'auto',
+          })
+        }
+      });
+
+      $(window).resize(function(){
+        if ($(window).width() > 943) {
+          equalheight('.featured-display .views-row');
+        }
+        else {
+          $('.featured-display .views-row').css({
+            'height': 'auto',
+          })
+        }
+      });
+    }
+  };
+
 })(jQuery);
