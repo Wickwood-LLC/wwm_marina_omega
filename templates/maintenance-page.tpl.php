98bd6b2b8baa127a211d3442edb3be30ad1f03ca
palagpatski <palagpatski@gmail.com>
2018-08-10 18:25:36 +0800

    Restore the logo

--- templates/maintenance-page.tpl.php
+++ templates/maintenance-page.tpl.php
@@ -47,11 +47,9 @@
                                     <div class="center-wrapper">
                                         <div class="panel-panel panel-col-first">
                                             <div class="inside">
-                                                <div class="panel-pane pane-page-logo">
-                                                    <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="site-logo">
-                                                        <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
-                                                    </a>
-                                                </div>
+                                                <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="site-logo">
+                                                    <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+                                                </a>
                                             </div>
                                         </div>
                                         <div class="panel-panel panel-col">
