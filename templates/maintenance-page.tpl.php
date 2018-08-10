7e5ae61db3dcb3a20dfcf3df58011fba0e4d96ae
palagpatski <palagpatski@gmail.com>
2018-08-10 19:32:17 +0800

    Add logo id

--- templates/maintenance-page.tpl.php
+++ templates/maintenance-page.tpl.php
@@ -47,7 +47,7 @@
                                     <div class="center-wrapper">
                                         <div class="panel-panel panel-col-first">
                                             <div class="inside">
-                                                <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="site-logo">
+                                                <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
                                                     <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
                                                 </a>
                                             </div>
