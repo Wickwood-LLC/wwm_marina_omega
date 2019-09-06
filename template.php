2f22c08ec7c20e80fdd56f0b3ba8c17c7dc394db
palagpatski <palagpatski@gmail.com>
2019-09-06 20:41:50 +0800

    Changing input submit to buttons

--- template.php
+++ template.php
@@ -123,10 +123,10 @@ function wwm_marina_omega_views_pre_render(&$view) {
 // }
 
 function wwm_marina_omega_form_alter(&$form, &$form_state, $form_id) {
-  dpm($form);
   if ($form['actions']['delete']) {
     $form['actions']['delete']['#value'] = "&#xf2ed; " . $form['actions']['delete']['#value'];
   }
+  dpm($form);
 }
 
 
