449bba8b1b97f553cfbf99172947e94c55d69164
palagpatski <palagpatski@gmail.com>
2019-09-09 23:04:32 +0800

    Add edit icon based on keyword

--- template.php
+++ template.php
@@ -105,7 +105,7 @@ function wwm_marina_omega_button(&$variables) {
     'value',
   ));
 
-  if ($element['#value'] == 'Edit selected') {
+  if (strpos($element['#value'], 'edit')) {
     $element['#attributes']['value'] = decode_entities('&#xf044; ') . $element['#attributes']['value'];
   }
   if ($element['#value'] == 'Delete selected') {
